04c967e6eb772a4685da87e43735c7963dbf936a
Mаксимов Илья <ilya.maximov11@gmail.com>
2023-02-13 14:10:14 +0300

    Add project files.

---
+++ personal-account-data/AccountDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using personal_account_data.DbEntities.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace personal_account_data
+{
+    public class AccountDbContext : IdentityDbContext<User, Role, string>
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<User> Roles { get; set; }
+    }
+}
---
+++ personal-account-data/DbEntities/User/User.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace personal_account_data.DbEntities.User
+{
+    public class User : IdentityUser
+    {
+        public User()
+        {
+
+        }
+    }
+}
